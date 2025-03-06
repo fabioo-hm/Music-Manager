2676e665306f6ff8c379936b48420402071858d5
fabioo-hm <194815533+fabioo-hm@users.noreply.github.com>
2025-03-06 16:13:02 -0500

    Primer Commit

---
+++ modules/ui.py
@@ -0,0 +1,38 @@
+MAIN_MENU = """
+===========================================
+        Administrador de Perfiles
+===========================================
+1. Crear un Nuevo Perfil
+2. Eliminar Perfil
+3. Administrar Perfiles
+4. Editar Perfil
+5. Salir
+===========================================
+Selecciona una opción (1-5):
+"""
+MENU_ADMIN = """
+===========================================
+            Administrar Perfiles
+===========================================
+¿Qué deseas hacer?
+1. Crear Playlist
+2. Seleccionar Playlist
+3. Eliminar Playlist
+4. Mostrar Playlist
+5. Regresar al Menú Principal
+===========================================
+Selecciona una opción (1-5):
+"""
+MENU_PLAYLIST = """
+===========================================
+            Administrar Playlist
+===========================================
+¿Qué deseas hacer?
+1. Agregar Canción
+2. Eliminar Canción
+3. Buscar Canción
+4. Mostrar Canciones
+5. Regresar al Menú Principal
+===========================================
+Selecciona una opción (1-5):
+"""
